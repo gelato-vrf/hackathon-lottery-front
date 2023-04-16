c7fb947e4c6c8ca4e1afb8cbc5c7130fba9968cd
Pierre <pierre.vinet17@gmail.com>
2023-04-16 10:43:13 +0200

    up

--- components/HeroCard.jsx
+++ components/HeroCard.jsx
@@ -6,7 +6,7 @@ const HeroCard = ({ account, setBuy, buy }) => {
   return (
     <div className="w-full flex flex-col items-center justify-center gap-10 p-10">
       <h1 className="text-4xl text-black">Get your ticket NOW!</h1>
-      <div className="flex flex-col items-center gap-1">
+      {/* <div className="flex flex-col items-center gap-1">
         <div className="flex flex-row gap-2 items-center">
           <p className="text-lg font-semibold">POT</p>
           <GiTwoCoins className="w-6 h-6" />
@@ -14,7 +14,7 @@ const HeroCard = ({ account, setBuy, buy }) => {
         <p className=" rounded-full p-2 px-6 text-2xl font-bold bg-black/40 text-white select-none">
           100 ETH
         </p>
-      </div>
+      </div> */}
       <div className="">
         {account.isConnected ? (
           <button

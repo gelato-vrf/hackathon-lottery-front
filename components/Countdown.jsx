d0be7bc734ad70cbcceb5ca4c3c750426cb3c5a3
Pierre <pierre.vinet17@gmail.com>
2023-04-16 11:06:16 +0200

    seconds

--- components/Countdown.jsx
+++ components/Countdown.jsx
@@ -9,7 +9,7 @@ const Countdown = () => {
       .then((result) => {
         console.log("_countdown", result);
         if (result) {
-          setCountDown(Math.round(result / 60));
+          setCountDown(result);
         }
       })
       .catch((error) => {
@@ -23,7 +23,7 @@ const Countdown = () => {
         <div className="px-7 py-3 bg-white z-10 rounded-xl text-lg flex flex-row items-center relative gap-3">
           <BiTimeFive className="w-8 h-8" />
           <p>
-            <span className="font-semibold">{countDown} minutes </span> until
+            <span className="font-semibold">{countDown} seconds </span> until
             the end.
           </p>
         </div>

067d64d5a7cb8867e2909ab48dd25fad717a9c0e
Pierre <pierre.vinet17@gmail.com>
2023-04-16 11:02:00 +0200

    up

--- components/TicketBought.jsx
+++ components/TicketBought.jsx
@@ -3,10 +3,12 @@ import { getWinner } from "../lib/functionsFromContract";
 
 const TicketBought = () => {
   const [winner, setWinner] = useState("");
+  const [random, setRandom] = useState(0);
   const handleCheckWinner = async () => {
     getWinner()
       .then((result) => {
-        setWinner(result);
+        setWinner(result.winner);
+        setRandom(result.rand);
       })
       .catch((error) => {
         console.error(error);
@@ -22,6 +24,11 @@ const TicketBought = () => {
       <p id="winner" className="font-semibold px-4 py-2 bg-white rounded-lg">
         {winner}
       </p>
+      {winner ? (
+        <p className="">
+          The random number is: <span className="">{random}</span>
+        </p>
+      ) : null}
       <button
         className="rounded-2xl border-4 border-blue-500 bg-transparent hover:bg-blue-500 text-blue-600 hover:text-white text-lg px-8 py-3 hover:scale-[1.02] duration-100"
         onClick={handleCheckWinner}
